2058607ccc2808427f5fc2d3a7952b2546ce2fef
eamaster <smah0085@student.monash.edu>
2025-11-08 18:55:17 +0800

    chore: Remove auto-generated .svelte-kit files from git tracking
    These files are auto-generated by SvelteKit during build/dev and should not be committed. They are already in .gitignore but were previously tracked. Removing them to prevent unnecessary merge conflicts and repository bloat.

--- .svelte-kit/generated/client/nodes/5.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/mood-impact-chart/+page.svelte";
--- .svelte-kit/generated/client/nodes/8.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../src/routes/sentiment-analyzer/+page.svelte";
